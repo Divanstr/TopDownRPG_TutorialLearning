4a9a790d5a815ce7443c2403ef72d0ef4e050412
Divanstr <anujkillerman@gmail.com>
2024-03-06 09:25:32 +0530

    first commit

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Learning_UE5
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Learning_UE5
+
+#pragma once
+
+#include "CoreMinimal.h"
+
